465a770dac5e5f9f44e4ce9860e639c2b409bfcb
Joe Carpenito <jcarpenito@sapient.com>
2017-01-27 15:18:46 -0500

    dont save ref to cursor

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ let MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/test', function (err, db) {
   if (err) throw err;
 
-  db.createCollection('mammals');
-
-  db.collection('mammals').insert({
-    name:'Bear',
-    size:'large'
-  });
-
-  let aCursor = db.collection('mammals').find().toArray(function(err, docs) {
+  db.collection('mammals').find().toArray(function(err, docs) {
     if (err) throw err;
 
     console.log(docs);
