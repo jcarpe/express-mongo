a6c0beab1716c1c4064e2281ed4cbb73af55b40c
Joe Carpenito <jcarpenito@sapient.com>
2017-02-10 16:29:50 -0500

    expand mogoose example

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 'use strict';
 
-let mongoose = require('mongoose');
+let mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost:27017/test');
 
-var db = mongoose.connection;
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
   console.log('mongoose connected to test db');
 });
+
+let fishSchema = new Schema({
+      name: String,
+      type: String,
+      size: String,
+    }),
+    Fish = mongoose.model('Fish', fishSchema),
+    silence = new Fish({ name: 'Silence' });
+
+console.log(silence.name);
