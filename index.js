ea6db627f35f480ad2ef126700a846acc80bdc91
Joe Carpenito <jcarpenito@sapient.com>
2017-02-10 14:59:01 -0500

    create mongoose example

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('mongoose connected to test db');
+});
